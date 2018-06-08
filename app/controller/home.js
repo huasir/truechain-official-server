1929b882e0e773f822a5cc98c878a5dce31c3118
baixiaojian <15101661380@163.com>
2018-06-08 18:04:46 +0800

    add cors to dependencies

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,11 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, egg';
+    this.ctx.body = {
+      code: 0,
+      message: 'hi, egg',
+      data: null
+    };
   }
 }
 
