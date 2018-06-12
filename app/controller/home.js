743654d2f77a9a77452065f914d3c1dc24546615
baixiaojian <15101661380@163.com>
2018-06-12 20:18:11 +0800

    fix upload method (Promise)

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,41 +3,50 @@
 const Controller = require('egg').Controller;
 const qiniu = require('qiniu');
 const path = require('path');
-var crypto = require('crypto');
-var md5 = crypto.createHash('md5');
+const crypto = require('crypto');
+const md5 = crypto.createHash('md5');
 const FormStream = require('formstream');
 const awaitWriteStream = require('await-stream-ready').write;
 const sendToWormhole = require('stream-wormhole');
+const toArray = require('stream-to-array');
 class HomeController extends Controller {
+  async promiseUploader (uploadToken, key, stream, putExtra, formUploader) {
+    return new Promise(function(resolve, reject) {
+      formUploader.putStream(uploadToken, key, stream, putExtra, function(respErr,
+        respBody, respInfo) {
+        if (respErr) {
+          reject(respErr);
+        } else {
+          resolve(respBody, respInfo)
+        }
+      });
+    });
+  }
   async index() {
     this.ctx.body = {
       code: 0,
-      message: 'hi, egg',
+      message: '后飒飒色',
       data: null
     };
+
   }
-  async test() {
-    const parts = this.ctx.multipart({ autoFields: true });
-    const thisFile = await parts();
-    // console.log(sss);
+  async uploaad() {
     const { ctx, app, config:{ qiniuUpload: { accessKey, secretKey } } } = this
-    // const { accessKey, secretKey } = this.config;
+    let stream       = await ctx.getFileStream();
     const config       = new qiniu.conf.Config();
     const options      = {
       scope: 'photo',
     };
-
     config.zone        = qiniu.zone.Zone_z2;
     const putPolicy    = new qiniu.rs.PutPolicy(options);
     const mac          = new qiniu.auth.digest.Mac(accessKey, secretKey);
     const uploadToken  = putPolicy.uploadToken(mac);
-    const localFile    = "/Users/baixiaojian/Desktop/fun/v2-17586bb5ff34ad3d9097c42658a53ad8_r.jpg";
-
     const formUploader = new qiniu.form_up.FormUploader(config);
     const putExtra     = new qiniu.form_up.PutExtra();
-    const key          = md5.update(`${ +new Date() }`).digest('hex');
-    const postfix      = thisFile.filename.match(/\.\w+$/)[0];
-    const imgSrc       = `http: //oo0yvx4fd.bkt.clouddn.com/${ key + postfix }`;
+    // const key          = md5.update(`${ +new Date() }`).digest('hex');
+    const key          =  `photo${+new Date()}`;
+    const postfix      = stream.filename.match(/\.\w+$/)[0];
+    const imgSrc       = `http://oo0yvx4fd.bkt.clouddn.com/${ key + postfix }`;
 
     if(!(['.jpg','.png'].includes(postfix))) {
       ctx.status = 402;
@@ -48,19 +57,29 @@ class HomeController extends Controller {
       }
       return;
     }
+    // debugger
+    try {
+      const { hash } = await this.promiseUploader(uploadToken, `${key + postfix}` , stream, putExtra, formUploader)
+      if(hash) {
+        ctx.body = {
+          code: 0,
+          message: '成功',
+          data: {
+            location: stream.filename,
+            imgSrc
+          }
+        }
+      } else {
+        ctx.status = 502;
+        ctx.body = {
+          code: 502,
+          message: '上传错误',
+          data: null
+        }
+      }
 
-    formUploader.putFile(uploadToken, `${key + postfix}` , localFile, putExtra, function(respErr,
-      respBody, respInfo) {
-      if (respErr) {
-        throw respErr;
-      }
-    });
-    ctx.body = {
-      code: 0,
-      message: '成功',
-      data: {
-        imgSrc
-      }
+    } catch (err) {
+      throw err;
     }
 
   }
