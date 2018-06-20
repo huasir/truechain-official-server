9e2d4c557f132872a90c8d0ab41cb19de1e077e9
baixiaojian <15101661380@163.com>
2018-06-20 17:57:18 +0800

    set pagination

---
+++ app/controller/main.js
@@ -0,0 +1,49 @@
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx, app } = this;
+    const { theme = 1, pageNumber = 10, pageIndex = 0 } = ctx.query;
+    // debugger
+    const result = await app.mysql.query(`
+      SELECT * FROM article
+      WHERE theme='${theme}'
+      ORDER BY
+      create_time
+      DESC
+      LIMIT
+      ${pageIndex}, ${pageNumber}
+    `);
+    ctx.body = {
+      code: 0,
+      message: '文章列表',
+      data: result
+    }
+  }
+  async detail() {
+    const { ctx, app } = this;
+    const { id  } = ctx.query;
+    if(!id) {
+      ctx.body = {
+        code: 422,
+        message: '参数不全',
+        data: null
+      }
+      return;
+    }
+    // debugger
+    const result = await app.mysql.query(`
+      SELECT a.*, c.content FROM content as c
+      INNER JOIN article as a
+      ON c.sid = '${id}'
+      AND a.id = '${id}'
+    `);
+    ctx.body = {
+      code: 0,
+      message: '文章列表',
+      data: result
+    }
+  }
+}
+
+module.exports = HomeController;
