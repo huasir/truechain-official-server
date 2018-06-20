059fe8ed59624c7882313e190a7dd84527ea58d6
baixiaojian <15101661380@163.com>
2018-06-20 17:56:16 +0800

    feat article router config

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ module.exports = app => {
     controller
   } = app;
   router.get('/', controller.home.index);
+  router.get('/articleList', controller.main.index);
+  router.get('/articleDetail', controller.main.detail);
   router.post('/uploaad', controller.home.uploaad);
   router.resources('topics', '/api/v2/topics', controller.topics);
 };
