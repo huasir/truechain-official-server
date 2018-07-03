547a88e77f0c9d9dd2c17e20b1fb4de5f2fa1f67
baixiaojian <15101661380@163.com>
2018-07-04 00:15:13 +0800

    add hash value

--- app/service/record.js
+++ app/service/record.js
@@ -5,8 +5,8 @@ const { aesEncrypt } = require('../util');
 class TopicService extends Service {
   async create(params) {
     const result = await this.app.mysql.get('db2').query(`
-      INSERT INTO record(address, value, cause, wechat, mobile, supply, create_time)
-      VALUES ('${params.address}', '${params.value}', '${params.cause || ''}', '${params.wechat || ''}', '${params.mobile || ''}', '${params.supply || ''}', '${+new Date()}')
+      INSERT INTO record(address, value, cause, wechat, mobile, supply, hash, create_time)
+      VALUES ('${params.address}', '${params.value}', '${params.cause || ''}', '${params.wechat || ''}', '${params.mobile || ''}', '${params.supply || ''}', '${ hash || ''}','${+new Date()}')
     `);
     if(result.affectedRows === 1) {
       return {
