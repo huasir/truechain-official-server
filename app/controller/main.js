1f06066358388e34a18d074d4d72ca53b77c75aa
baixiaojian <15101661380@163.com>
2018-08-29 19:55:23 +0800

    fix bug

--- app/controller/main.js
+++ app/controller/main.js
@@ -4,18 +4,15 @@ class HomeController extends Controller {
   async index() {
     const { ctx, app } = this;
     const { theme = 1, pageNumber = 10, pageIndex = 0, language = 1 } = ctx.query;
-    // debugger
     const result = await app.mysql.get('db1').query(`
-      SELECT * FROM article
-      WHERE language ='?'
-      ORDER BY
-      create_time
-      DESC
-      LIMIT
-      ?, ?
-    `,
-    [language,pageIndex,pageNumber]
-    );
+        SELECT * FROM article
+        WHERE language='${language}'
+        ORDER BY
+        create_time
+        DESC
+        LIMIT
+        ${pageIndex}, ${pageNumber}
+    `);
     ctx.body = {
       code: 0,
       message: '文章列表',
@@ -25,7 +22,7 @@ class HomeController extends Controller {
 
   async detail() {
     const { ctx, app } = this;
-    const { id  } = ctx.query;
+    const { id } = ctx.query;
     if(!id) {
       ctx.body = {
         code: 422,
@@ -62,8 +59,8 @@ class HomeController extends Controller {
     }
     else{
       ctx.status = 204;
-    }    
-    
+    }
+
   }
 
   async getIpInfo() {
