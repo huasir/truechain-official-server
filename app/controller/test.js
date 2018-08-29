1f3955fe562016474aa17477cae8eb0c0a456bfe
baixiaojian <15101661380@163.com>
2018-08-29 19:16:08 +0800

    update test code

--- app/controller/test.js
+++ app/controller/test.js
@@ -5,15 +5,15 @@ const Service = require('egg').Service;
 class Test extends Service {
   async index() {
     const { ctx, app } = this;
-    const { pageIndex = 0, pageNumber = 300 } = ctx.query;
-    const result = await app.mysql.get('db3').query(`
-      SELECT * FROM add_d
-      LIMIT ${pageIndex}, ${pageNumber}
-    `)
+    // const { pageIndex = 0, pageNumber = 300 } = ctx.query;
+    // const result = await app.mysql.get('db3').query(`
+    //   SELECT * FROM add_d
+    //   LIMIT ${pageIndex}, ${pageNumber}
+    // `)
     this.ctx.body = {
       code: 0,
       message: '成功',
-      data: result
+      data: null
     }
   }
 }
